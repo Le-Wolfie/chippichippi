2ceaf96b82a506b3c653ec1b7ad17a0d7d744172
Amr Khaled <lewolfje@gmail.com>
2024-01-14 00:14:21 +0200

    Add audio playback

--- popup.js
+++ popup.js
@@ -3,5 +3,7 @@ document
   .addEventListener("click", function () {
     browser.tabs.query({ active: true, currentWindow: true }, function (tabs) {
       browser.tabs.sendMessage(tabs[0].id, { action: "replaceImages" });
+      // Send a message to the background script to play the audio
+      browser.runtime.sendMessage({ action: "playAudio" });
     });
   });
